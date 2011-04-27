db340f187da7b6520ca867f0fb7740784027f08e
Eder Sousa <sous776@hopper01.(none)>
2011-04-27 15:40:42 -0700

    Preprocess and start runsfor Multilevel Monte Carlo.

---
+++ mmc_runs_setup.py
@@ -0,0 +1,99 @@
+import os
+import random
+import string
+	
+
+# get paramenters that remain the same for all runs
+f = open('input.py', 'r')
+info = f.read()
+f.close
+
+# Number of runs
+last = 1  # number of runs
+
+for count in range(last):
+	# uniform varying values of mass ration from
+	# a to b given a certain value of ME
+	v = random.uniform(25,100)
+	me = 0.01
+	mi = v * me
+
+	# uniform varying values of the speed of light
+	# c = random.uniform(8,12)
+	c = 1.00
+
+	light = ("LIGHT_SPEED =" + str(c))
+	masse = ("ME =" + str(me))
+	massi = ("MI =" + str(mi))
+
+	folder = ("recon_006_MR_" + str(v) + "_c0_" + str(c))
+
+
+	## create Warpx .pin file
+	out = open('ssrecon_wv_0.pin','w')
+	## write data into .pin file
+	out.write('# -*- python -*- \n')
+	out.write('# The following parameters has been randomly generated. \n')
+	out.write(light)
+	out.write("\n")
+	out.write(masse)
+	out.write("\n")
+	out.write(massi)
+	out.write("\n")
+	out.write('# -- End of randomly generated data. --')
+	out.write("rname = ssrecon_wv_0")
+	out.write("\n")
+	out.write("nx = 400")
+	out.write("\n")
+	out.write("ny = 200")
+	out.write("\n")
+	out.write(info)
+	out.close
+
+	os.mkdir(folder)
+	os.system("mv ssrecon_wv_0.pin " + folder)
+	os.system("cp cray_0.qsub " + folder)
+
+	if count <= round(last/2):
+		out = open('ssrecon_wv_1.pin','w')
+		out.write('# -*- python -*- \n')
+		out.write('# The following parameters has been randomly generated. \n')
+		out.write(light)
+		out.write("\n")
+		out.write(masse)
+		out.write("\n")
+		out.write(massi)
+		out.write("\n")
+		out.write('# -- End of randomly generated data. --')
+		out.write("rname = ssrecon_wv_1")
+		out.write("\n")
+		out.write("nx = 800")
+		out.write("\n")
+		out.write("ny = 400")
+		out.write("\n")
+		out.write(info)
+		out.close
+		os.system("mv ssrecon_wv_1.pin " + folder)
+		os.system("cp cray_1.qsub " + folder)
+
+	if count <= round(last/4):
+		out = open('ssrecon_wv_2.pin','w')
+		out.write('# -*- python -*- \n')
+		out.write('# The following parameters has been randomly generated. \n')
+		out.write(light)
+		out.write("\n")
+		out.write(masse)
+		out.write("\n")
+		out.write(massi)
+		out.write("\n")
+		out.write('# -- End of randomly generated data. --')
+		out.write("rname = ssrecon_wv_2")
+		out.write("\n")
+		out.write("nx = 1600")
+		out.write("\n")
+		out.write("ny = 800")
+		out.write("\n")
+		out.write(info)
+		out.close
+		os.system("mv ssrecon_wv_2.pin " + folder)
+		os.system("cp cray_2.qsub " + folder)
--- run_cases.sh
+++ run_cases.sh
@@ -1,7 +1,20 @@
-for f in recon_004_MR_*; do
+for f in recon_006_MR_*; do
     band=$(echo "$f")
     cd "$band"
-    $warpxs -i ssrecon_wv.inp
-#    gedit ssrecon_wv.inp &
+    $wxpp -i ssrecon_wv_0.pin
+    #qsub cray_0.qsub
+
+    if [ -a "ssrecon_wv_1.pin" ]
+	then
+	$wxpp -i ssrecon_wv_1.pin
+	#qsub cray_1.qsub
+    fi
+
+    if [ -a "ssrecon_wv_2.pin" ]
+	then
+	$wxpp -i ssrecon_wv_2.pin
+	#qsub cray_2.qsub
+    fi
+
     cd ../
 done
